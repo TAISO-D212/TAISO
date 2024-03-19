fe82aea935e14043ef2407f1a7c63d68f8cd4c53
김태용 <94teo@naver.com>
2024-03-19 17:08:19 +0900

    [FE] 프로젝트 기본 구성
     - 내용 : 홈페이지 로딩 추가

---
+++ FE/src/components/home/Loading.tsx
@@ -0,0 +1,10 @@
+import Logo from '../../assets/icon/icon_192.png';
+
+export const Loading = () => {
+  return (
+    <div className="flex-col justify-center items-center">
+      <img src={Logo} />
+      <h1>TAISO</h1>
+    </div>
+  );
+};
