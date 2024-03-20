099531df4a021c9275166a2ee4f07042eb0e7d92
김태용 <94teo@naver.com>
2024-03-20 15:17:06 +0900

    [FE] 로그인 화면 UI 추가
     - 로그인 화면 UI 추가
     - BE 연결 전

--- FE/src/router.tsx
+++ FE/src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { LoginPage } from './pages/LoginPage';
+import { LoginPage } from './pages/login/LoginPage.tsx';
 
 export const router = createBrowserRouter([
   {path: '/', element: <LoginPage />},
