795a9b3f4b4c828941c2708290932eaea1fbc8ec
배성연 <amysia@knu.ac.kr>
2024-03-27 10:27:58 +0900

    Update TaisoApplication.java

--- BE/taiso/src/main/java/com/d212/taiso/TaisoApplication.java
+++ BE/taiso/src/main/java/com/d212/taiso/TaisoApplication.java
@@ -9,10 +9,10 @@ public class TaisoApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        // Dotenv dotenv = Dotenv.load();
+        // dotenv.entries().forEach(entry -> {
+        //     System.setProperty(entry.getKey(), entry.getValue());
+        // });
 
         SpringApplication.run(TaisoApplication.class, args);
     }
