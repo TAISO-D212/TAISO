72d572cd553373040b16c6933616841c1c1257f7
김태용 <94teo@naver.com>
2024-04-02 18:00:45 +0900

    [FE] feat: FCM 프론트 기능 구현
     - 내용 : FCM 기본 파일 구성

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,6 +2,7 @@ import { MainLayout } from './layouts/MainLayout';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { watchPositionHook } from './hooks/watchPositionHook';
+import './firebase-messaging-sw.ts';
 
 export const App = () => {
 	watchPositionHook();
