5c1d5d56f355e1c1c692e5777ee952f01a743613
gyeongri <50294908+gyeongri@users.noreply.github.com>
2024-04-04 02:25:50 +0900

    [DevOps] docs: README.md 작성
    - 변경내용 1: README.md 작성함.

--- BE/taiso/src/main/java/com/d212/taiso/domain/member/service/AlertService.java
+++ BE/taiso/src/main/java/com/d212/taiso/domain/member/service/AlertService.java
@@ -1,17 +1,18 @@
 package com.d212.taiso.domain.member.service;
 
 import com.d212.taiso.domain.member.dto.AlertDto;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import org.hibernate.sql.exec.ExecutionException;
 
 public interface AlertService {
 
-    public void sendAlert(AlertDto alert) throws ExecutionException, InterruptedException;
+    public void sendAlert(AlertDto alert) throws ExecutionException, InterruptedException, FirebaseMessagingException;
 
-    public void departAlertSend(Long rsvId) throws ExecutionException, InterruptedException;
+    public void departAlertSend(Long rsvId) throws ExecutionException, InterruptedException, FirebaseMessagingException;
 
-    public void soonAlertSend(Long rsvId) throws ExecutionException, InterruptedException;
+    public void soonAlertSend(Long rsvId) throws ExecutionException, InterruptedException, FirebaseMessagingException;
 
-    public void arrivalAlertSend(Long rsvId) throws ExecutionException, InterruptedException;
+    public void arrivalAlertSend(Long rsvId) throws ExecutionException, InterruptedException, FirebaseMessagingException;
 
 //    public String getNotificationToken();
 }
